0dd85373afddb49932aa21669bf268864dc33aa9
ItsPaulHawkins <hawkinsanthonypaul@gmail.com>
2017-05-21 00:48:27 -0400

    u g h

--- AI/Program.cs
+++ AI/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace AI
 {
@@ -10,6 +11,36 @@ namespace AI
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Alec: Hello! I'm Alec, your personal assistant.");
+            string input = Console.ReadLine();
+            string[] splitInput = input.Split(' ');
+            bool found = false;
+            int place = 0;
+            while (found == false)
+            {
+                if(splitInput[place] == "images" || splitInput[place] == "image" || splitInput[place] == "Image" || splitInput[place] == "Images" || splitInput[place] == "picture" || splitInput[place] == "pictures" || splitInput[place] == "Pictures" || splitInput[place] == "picture")
+                {
+                    found = true;
+                    string image = splitInput[place + 2];
+                    findImage(image);
+                }else
+                {
+                    place++;
+                }
+            }
+
+
+        }
+        public static void Sleep(int seconds)
+        {
+            Thread.Sleep(seconds * 1000);
+        }
+        public static string findImage(string image)
+        {
+            Console.WriteLine(image);
+            Console.ReadLine();
+
+            return image;
         }
     }
 }
